26b3c0d0d660c579172f482f6193d63d56e5bbfe
brunolucy2023x <lucyb5147@gmail.com>
2024-01-18 02:59:53 -0800

    Upadted

--- pint.c
+++ pint.c
@@ -1,9 +1,18 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * f_pint - prints the top
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * creator: Bruno Owino
+ * email: brunookoth44@gmail.com
+ * Return: Solution
 */
 void f_pint(stack_t **head, unsigned int counter)
 {
