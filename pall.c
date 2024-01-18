ce41706883784d13e955af88e64b1ffc52283b11
brunolucy2023x <lucyb5147@gmail.com>
2024-01-18 02:48:13 -0800

    Upadted

--- pall.c
+++ pall.c
@@ -1,8 +1,17 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * f_pall - prints the stack
  * @head: stack head
  * @counter: no used
+ * creator: Bruno Owino
+ * email: brunookoth44@gmail.com
  * Return: no return
 */
 void f_pall(stack_t **head, unsigned int counter)
