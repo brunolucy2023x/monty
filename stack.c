3247db670ebc0f1e6ecf7c3a48badfff7459256b
brunolucy2023x <lucyb5147@gmail.com>
2024-01-18 03:35:36 -0800

    Upadted

--- stack.c
+++ stack.c
@@ -1,8 +1,17 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * f_stack - prints the top
  * @head: stack head
  * @counter: line_number
+ * creator: Bruno Owino
+ * email: brunookoth44@gmail.com
  * Return: no return
 */
 void f_stack(stack_t **head, unsigned int counter)
