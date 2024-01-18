fb32814f98010926d1086e96d29d20ea25be8dfa
brunolucy2023x <lucyb5147@gmail.com>
2024-01-18 03:28:09 -0800

    Upadted

--- rotr.c
+++ rotr.c
@@ -1,8 +1,17 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 /**
   *f_rotr- rotates the stack to the bottom
   *@head: stack head
   *@counter: line_number
+  *creator: Bruno Owino
+  *email: brunookoth44@gmail.com
   *Return: no return
  */
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
