54d5a8721a6dd3dc0db301bacdf9f78b4a843c19
brunolucy2023x <lucyb5147@gmail.com>
2024-01-18 02:19:55 -0800

    Nineth Update

--- free_stack.c
+++ free_stack.c
@@ -1,7 +1,16 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 /**
 * free_stack - frees a doubly linked list
 * @head: head of the stack
+* creator: Bruno Owino
+* email: brunookoth44@gmail.com
 */
 void free_stack(stack_t *head)
 {
