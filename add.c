ee4c47ee6269b4929bb3e2dab328a77b9ad0fa8d
brunolucy2023x <lucyb5147@gmail.com>
2024-01-18 01:43:10 -0800

    Sixth Update

--- add.c
+++ add.c
@@ -1,16 +1,16 @@
 #include "monty.h"
 /**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
+ * f_sol - adds the top two elements of the stack.
+ * @top: stack head
+ * @sol: line_number
  * Return: no return
 */
-void f_add(stack_t **head, unsigned int counter)
+void f_sol(stack_t **top, unsigned int sol)
 {
 	stack_t *h;
 	int len = 0, aux;
 
-	h = *head;
+	h = *top;
 	while (h)
 	{
 		h = h->next;
@@ -18,15 +18,15 @@ void f_add(stack_t **head, unsigned int counter)
 	}
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't find answer too short\n", sol);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		free_stack(*top);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
+	h = *top;
 	aux = h->n + h->next->n;
 	h->next->n = aux;
-	*head = h->next;
+	*top = h->next;
 	free(h);
 }
